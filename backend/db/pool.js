b3ce8ef698d9c8bdb995d8f89520bd0c719ed99e
Muksiduz <muksiduzzaman124@gmail.com>
2026-07-06 13:49:22 +0530

    neon db

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,19 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new pg.Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-console.log({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-console.log(pool);
 
 export default pool;
